3552b26bed5f0d100c8225fd9e3f9f23883df714
AustinT12 <austintran85@gmail.com>
2022-11-26 14:35:08 -0500

    more designs and added an icon to featured compon

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,7 +1,22 @@
 import './featured.scss';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { CircularProgressbar } from "react-circular-progressbar";
+
 
 const Featured = () => {
-  return <div className="featured">Featured</div>;
+  return (
+    <div className='featured'>
+      <div className='top'>
+        <h1 className='title'>Total Revenue</h1>
+        <MoreVertIcon fontSize='small' />
+      </div>
+      <div className='bottom'>
+        <div className="featuredChart">
+
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
