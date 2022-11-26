af7eea4834cf161e9b29cc662ec4cfce0c2d4642
AustinT12 <austintran85@gmail.com>
2022-11-26 14:55:41 -0500

    recharts imports for chart component

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,10 +1,23 @@
 import './chart.scss';
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
 
-
+const data = [
+  { name: 'January', Total: 1200 },
+  { name: 'February', Total: 2100 },
+  { name: 'March', Total: 800 },
+  { name: 'April', Total: 1600 },
+  { name: 'May', Total: 900 },
+  { name: 'June', Total: 1700 },
+];
 const Chart = () => {
-  return (
-    <div className="chart">Chart</div>
-  )
-}
+  return <div className='chart'>Chart</div>;
+};
 
-export default Chart
+export default Chart;
