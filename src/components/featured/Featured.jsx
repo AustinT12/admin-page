28f662771c17bacc7f6265da0b6040b34de5a639
AustinT12 <austintran85@gmail.com>
2022-11-26 14:51:13 -0500

    finished styling featured component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,7 +1,9 @@
 import './featured.scss';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { CircularProgressbar } from "react-circular-progressbar";
-
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
 
 const Featured = () => {
   return (
@@ -11,8 +13,34 @@ const Featured = () => {
         <MoreVertIcon fontSize='small' />
       </div>
       <div className='bottom'>
-        <div className="featuredChart">
-
+        <div className='featuredChart'>
+          <CircularProgressbar value={70} text={'%70'} strokeWidth={5} />
+        </div>
+        <p className='title'>Total sales made today</p>
+        <p className='amount'>$420</p>
+        <p className='description'>Description here</p>
+        <div className='summary'>
+          <div className='item'>
+            <div className='itemTitle'>Today</div>
+            <div className='itemResult negative'>
+              <KeyboardArrowDownIcon fontSize='small' />
+              <div className='resultAmount'>$12k</div>
+            </div>
+          </div>
+          <div className='item'>
+            <div className='itemTitle'>Last week</div>
+            <div className='itemResult positive'>
+              <KeyboardArrowUpOutlinedIcon fontSize='small' />
+              <div className='resultAmount'>$5k</div>
+            </div>
+          </div>
+          <div className='item'>
+            <div className='itemTitle'>Last month</div>
+            <div className='itemResult positive'>
+              <KeyboardArrowUpOutlinedIcon fontSize='small' />
+              <div className='resultAmount'>$14k</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
