ed26bf6157f8935a2c7e89803f9d1e8f63ad80ac
AustinT12 <austintran85@gmail.com>
2022-12-12 13:03:08 -0500

    created a useState for user to add img file

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -2,8 +2,10 @@ import './new.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
+import { useState } from 'react';
 
 const New = ({ inputs, title }) => {
+  const [file, setFile] = useState('');
   return (
     <div className='new'>
       <Sidebar />
@@ -15,7 +17,12 @@ const New = ({ inputs, title }) => {
         <div className='bottom'>
           <div className='left'>
             <img
-              src='https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'
+              src={
+                // if there is a file select then it will display, if not the default image will display
+                file
+                  ? URL.createObjectURL(file)
+                  : 'https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'
+              }
               alt=''
             />
           </div>
@@ -26,7 +33,13 @@ const New = ({ inputs, title }) => {
                 <label htmlFor='file'>
                   Image: <DriveFolderUploadOutlinedIcon className='icon' />
                 </label>
-                <input type='file' id='file' style={{ display: 'none' }} />
+                <input
+                  type='file'
+                  id='file'
+                  // gets the first file
+                  onChange={(e) => setFile(e.target.file[0])}
+                  style={{ display: 'none' }}
+                />
               </div>
               {/* this maps all of the inputs from the formSource files using props defined in app.js (title and inputs) */}
               {inputs.map((input) => (
