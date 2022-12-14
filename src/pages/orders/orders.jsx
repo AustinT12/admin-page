e087a8a7f4652ac35a8b903bdef532565f4c3511
AustinT12 <austintran85@gmail.com>
2022-12-14 16:11:41 -0500

    Added table component to orders page

--- src/pages/orders/orders.jsx
+++ src/pages/orders/orders.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import './orders.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
+import Table from '../../components/table/Table';
+
 const orders = () => {
   return (
-    <div className='order'>
+    <div className='orders'>
       <Sidebar />
       <div className='orderContainer'>
         <Navbar />
+        <div className='ordersTable'>
+          <Table />
+        </div>
       </div>
     </div>
   );
